fcb57740c266804a4e4e9d0e8d5a62bc9d64e20f
Pedro Mendes <pedro.trabalho.uma@gmail.com>
2019-08-25 00:17:45 +0100

    fixed untracked files

--- src/DjangoFormSet.ts
+++ src/DjangoFormSet.ts
@@ -2,23 +2,29 @@ class Formset {
   private selector: string;
   private prefix: string;
 
-  private addElement: Element;
+  private totalFormsElement: HTMLElement;
+  private maxFormsElement: HTMLElement;
+  private minFormsElement: HTMLElement;
+
+  private addElement: HTMLElement;
   private addElementDefaultText: string;
   private addElementWrapperSelector: string;
 
   private canDelete: boolean;
-  private deleteElement: Element;
+  private deleteElement: HTMLElement;
   private deleteElementDefaultText: string;
   private deleteElementWrapperSelector: string;
 
   private canOrder: boolean;
-  private orderElement: Element;
-  private orderElementDefaultText: Object;
+  private orderElement: HTMLElement;
+  private orderElementBeforeSelector: string;
+  private orderElementBeforeDefaultText: string;
+  private orderElementAfterSelector: string;
+  private orderElementAfterDefaultText: string;
   private orderElementWrapperSelector: string;
 
-  private formsWrapper: Element;
-  private forms: NodeListOf<Element>;
-  private formTemplate: Element;
+  private formsWrapper: HTMLElement;
+  private formTemplate: HTMLElement;
 
   private beforeAddEventName = "beforeadd";
   private beforeAddEvent: CustomEvent;
@@ -39,25 +45,35 @@ class Formset {
     selector: string,
     prefix: string = "",
 
-    addElement: Element = null,
+    addElement: HTMLElement = null,
     addElementDefaultText: string = "Add",
-    addElementWrapperSelector: string,
+    addElementWrapperSelector: string = "formset-wrapper-add",
 
     canDelete: boolean = false,
-    deleteElement: Element = null,
+    deleteElement: HTMLElement = null,
     deleteElementDefaultText: string = "Delete",
-    deleteElementWrapperSelector: string,
+    deleteElementWrapperSelector: string = "formset-wrapper-delete",
 
     canOrder: boolean = false,
-    orderElement: Element = null,
-    orderElementDefaultText: Object = {
-      before: "Before",
-      after: "After"
-    },
-    orderElementWrapperSelector: string
+    orderElement: HTMLElement = null,
+    orderElementBeforeSelector: string = "formset-order-before",
+    orderElementBeforeDefaultText: string = "Before",
+    orderElementAfterSelector: string = "formset-order-after",
+    orderElementAfterDefaultText: string = "After",
+    orderElementWrapperSelector: string = "formset-wrapper-order"
   ) {
     this.selector = selector;
     this.prefix = prefix;
+
+    this.totalFormsElement = document.getElementById(
+      `id_${this.prefix}-TOTAL_FORMS`
+    );
+    this.minFormsElement = document.getElementById(
+      `id_${this.prefix}-MIN_NUM_FORMS`
+    );
+    this.maxFormsElement = document.getElementById(
+      `id_${this.prefix}-MAX_NUM_FORMS`
+    );
 
     this.addElement = addElement || this.getDefaultAddElement();
     this.addElementDefaultText = addElementDefaultText;
@@ -66,34 +82,84 @@ class Formset {
     this.canDelete = canDelete;
     this.deleteElement = deleteElement || this.getDefaultDeleteElement();
     this.deleteElementDefaultText = deleteElementDefaultText;
+    this.deleteElementWrapperSelector = deleteElementWrapperSelector;
 
     this.canOrder = canOrder;
-    this.orderElement = orderElement || this.getDefaultOrderElement();
-    this.orderElementDefaultText = orderElementDefaultText;
-    this.orderElement;
+    this.orderElement = orderElement;
+    this.orderElementBeforeSelector = orderElementBeforeSelector;
+    this.orderElementBeforeDefaultText = orderElementBeforeDefaultText;
+    this.orderElementAfterSelector = orderElementAfterSelector;
+    this.orderElementAfterDefaultText = orderElementAfterDefaultText;
+    this.orderElementWrapperSelector = orderElementWrapperSelector;
 
-    this.forms = document.querySelectorAll(this.selector);
-    if (this.forms.length) {
-      let formReference = this.forms[0];
+    var forms = this.getForms();
+    if (forms.length) {
+      var formReference = forms[0];
       this.formsWrapper = formReference.parentElement;
       this.formTemplate = this.cleanForm(formReference);
     } else {
       throw "Must have atleast one form to create a template!";
     }
 
-    this.beforeAddEvent = new CustomEvent(this.beforeAddEventName) || null;
-    this.afterAddEvent = new CustomEvent(this.afterAddEventName) || null;
+    this.beforeAddEvent = new CustomEvent(this.beforeAddEventName);
+    this.afterAddEvent = new CustomEvent(this.afterAddEventName);
 
-    this.beforeDeleteEvent =
-      new CustomEvent(this.beforeDeleteEventName) || null;
-    this.afterDeleteEvent = new CustomEvent(this.afterDeleteEventName) || null;
+    if (this.canDelete) {
+      this.beforeDeleteEvent = new CustomEvent(this.beforeDeleteEventName);
+      this.afterDeleteEvent = new CustomEvent(this.afterDeleteEventName);
+    }
 
-    this.beforeOrderEvent = new CustomEvent(this.beforeOrderEventName) || null;
-    this.afterOrderEvent = new CustomEvent(this.afterOrderEventName) || null;
+    if (this.canOrder) {
+      this.beforeOrderEvent = new CustomEvent(this.beforeOrderEventName);
+      this.afterOrderEvent = new CustomEvent(this.afterOrderEventName);
+    }
   }
 
-  cleanForm(form: Element): Element {
-    var formTemplate = (form.cloneNode(true) as Element) as Element;
+  getIntValueMethodFormat(element: HTMLElement): number {
+    return parseInt(element.getAttribute("value"));
+  }
+
+  setIntValueMethodFormat(element: HTMLElement, value: number): void {
+    element.setAttribute("value", value.toString());
+  }
+
+  getNumberOfTotalForms(): number {
+    return this.getIntValueMethodFormat(this.totalFormsElement);
+  }
+
+  getNumberOfMinForms(): number {
+    return this.getIntValueMethodFormat(this.minFormsElement);
+  }
+
+  getNumberOfMaxForms(): number {
+    return this.getIntValueMethodFormat(this.maxFormsElement);
+  }
+
+  setupElementInFormBySelector(
+    form: HTMLElement,
+    element: HTMLElement,
+    selector: string
+  ): void {
+    var elementWrapper = form.querySelector(selector);
+    elementWrapper.append(element);
+  }
+
+  decrementTotalForms(): void {
+    var value = parseInt(this.totalFormsElement.getAttribute("value"));
+    this.totalFormsElement.setAttribute("value", (value - 1).toString());
+  }
+
+  incrementTotalForms(): void {
+    var value = parseInt(this.totalFormsElement.getAttribute("value"));
+    this.totalFormsElement.setAttribute("value", (value + 1).toString());
+  }
+
+  getForms(): NodeListOf<HTMLElement> {
+    return document.querySelectorAll(this.selector);
+  }
+
+  cleanForm(form: HTMLElement): HTMLElement {
+    var formTemplate = form.cloneNode(true) as HTMLElement;
     var inputs = formTemplate.querySelectorAll(":input");
     for (let index = 0; index < inputs.length; index++) {
       const element = inputs[index];
@@ -102,71 +168,230 @@ class Formset {
     return formTemplate;
   }
 
-  getDefaultAddElement(): Element {
+  getDefaultAddElement(): HTMLElement {
     var addButton = document.createElement("button");
     addButton.setAttribute("type", "button");
     addButton.textContent = this.addElementDefaultText;
     return addButton;
   }
 
-  setupAddNode(): Node {
-    var clonedElement = this.addElement.cloneNode(true) as Element;
+  getSetupAddElement(): HTMLElement {
+    var clonedElement = this.addElement.cloneNode(true) as HTMLElement;
     clonedElement.addEventListener("click", () => {
       clonedElement.dispatchEvent(this.beforeAddEvent);
-      this.onAddElement(clonedElement);
+      this.onAddingElement(clonedElement);
       clonedElement.dispatchEvent(this.afterAddEvent);
     });
     return clonedElement;
   }
 
-  onAddElement(node: Element): void {}
+  onAddingElement(element: HTMLElement): void {
+    var newElement = this.formTemplate.cloneNode(true) as HTMLElement;
+    this.setupElement(newElement, this.getNumberOfTotalForms() + 1);
+    this.formsWrapper.append(newElement);
+    this.incrementTotalForms();
+    this.update();
+  }
 
-  getDefaultDeleteElement(): Element {
+  getDefaultDeleteElement(): HTMLElement {
     var deleteButton = document.createElement("button");
     deleteButton.setAttribute("type", "button");
     deleteButton.textContent = this.deleteElementDefaultText;
     return deleteButton;
   }
 
-  setupDeleteElement(): Element {
-    var clonedElement = this.deleteElement.cloneNode(true) as Element;
+  getSetupDeleteElement(): HTMLElement {
+    var clonedElement = this.deleteElement.cloneNode(true) as HTMLElement;
     clonedElement.addEventListener("click", () => {
       clonedElement.dispatchEvent(this.beforeDeleteEvent);
-      this.onDeleteElement(clonedElement);
+      this.onDeletingElement(clonedElement);
       clonedElement.dispatchEvent(this.afterDeleteEvent);
     });
     return clonedElement;
   }
 
-  onDeleteElement(node: Element): void {}
+  onDeletingElement(element: HTMLElement): void {
+    var form = element.closest(this.selector);
+    form.remove();
+    this.decrementTotalForms();
+    this.update();
+  }
 
-  getDefaultOrderElement(): Element {
+  getDefaultOrderElement(): HTMLElement {
     var wrapper = document.createElement("div");
 
     var beforeButton = document.createElement("button");
     beforeButton.setAttribute("type", "button");
-    beforeButton.textContent = this.orderElementDefaultText["before"];
+    beforeButton.textContent = this.orderElementBeforeDefaultText;
+    wrapper.append(beforeButton);
 
     var afterButton = document.createElement("button");
     afterButton.setAttribute("type", "button");
-    afterButton.textContent = this.orderElementDefaultText["after"];
-
-    wrapper.append(beforeButton, afterButton);
+    afterButton.textContent = this.orderElementAfterDefaultText;
+    wrapper.append(afterButton);
 
     return wrapper;
   }
 
-  setupOrderElement(): Element {
-    var clonedElement = this.addElement.cloneNode(true) as Element;
-    clonedElement.addEventListener("click", () => {
+  getSetupOrderElement(): HTMLElement {
+    var clonedElement = this.orderElement.cloneNode(true) as HTMLElement;
+    var beforeElement = clonedElement.querySelector(
+      this.orderElementBeforeSelector
+    );
+    beforeElement.addEventListener("click", () => {
       clonedElement.dispatchEvent(this.beforeOrderEvent);
-      this.onOrderElement(clonedElement);
+      this.onMovingBefore(clonedElement);
+      clonedElement.dispatchEvent(this.afterOrderEvent);
+    });
+
+    var afterElement = clonedElement.querySelector(
+      this.orderElementAfterSelector
+    );
+    afterElement.addEventListener("click", () => {
+      clonedElement.dispatchEvent(this.beforeOrderEvent);
+      this.onMovingAfter(clonedElement);
       clonedElement.dispatchEvent(this.afterOrderEvent);
     });
     return clonedElement;
   }
 
-  onOrderElement(node: Element): void {}
+  insertAfter(newElement: HTMLElement, referenceElement: HTMLElement) {
+    referenceElement.parentNode.insertBefore(
+      newElement,
+      referenceElement.nextSibling
+    );
+  }
 
-  setup(): void {}
+  onMovingAfter(element: HTMLElement) {
+    var form = element.closest(this.selector);
+    var clonedForm = form.cloneNode(true) as HTMLElement;
+    var forms = this.getForms();
+    var referenceElement = forms[this.getIndex(forms, form) + 1];
+    form.remove();
+    this.insertAfter(clonedForm, referenceElement);
+  }
+
+  onMovingBefore(element: HTMLElement) {
+    var form = element.closest(this.selector);
+    var clonedForm = form.cloneNode(true) as HTMLElement;
+    var forms = this.getForms();
+    var referenceElement = forms[this.getIndex(forms, form) - 1];
+    form.remove();
+    this.insertAfter(clonedForm, referenceElement);
+  }
+
+  getIndex(elementList, elementReference) {
+    for (let index = 0; index < elementList.length; index++) {
+      const element = elementList[index];
+      if (element == elementReference) {
+        return index;
+      }
+    }
+    return -1;
+  }
+
+  setup(): void {
+    var forms = this.getForms();
+    for (let index = 1; index <= forms.length; index++) {
+      const element = forms[index];
+      this.setupElement(element, index);
+    }
+  }
+
+  update(): void {
+    var forms = this.getForms();
+    for (let index = 1; index <= forms.length; index++) {
+      const element = forms[index];
+      this.updateElement(element, index);
+    }
+  }
+
+  updateElement(formElement: HTMLElement, index: number): void {
+    this.updateAddElement(formElement, index);
+    if (this.canDelete) {
+      this.updateDeleteElement(formElement, index);
+    }
+    if (this.canOrder) {
+      this.updateOrderElement(formElement, index);
+    }
+  }
+
+  setupElement(formElement: HTMLElement, index: number): void {
+    var addElement = this.getSetupAddElement();
+    this.setupElementInFormBySelector(
+      formElement,
+      addElement,
+      this.addElementWrapperSelector
+    );
+    this.updateAddElement(formElement, index);
+
+    if (this.canDelete) {
+      var deleteElement = this.getSetupDeleteElement();
+      this.setupElementInFormBySelector(
+        formElement,
+        deleteElement,
+        this.deleteElementWrapperSelector
+      );
+      this.updateDeleteElement(formElement, index);
+    }
+
+    if (this.canOrder) {
+      var orderElement = this.getSetupOrderElement();
+      this.setupElementInFormBySelector(
+        formElement,
+        orderElement,
+        this.orderElementWrapperSelector
+      );
+      this.updateOrderElement(formElement, index);
+    }
+  }
+
+  setVisibility(htmlElement: HTMLElement, isVisible: boolean): void {
+    if (isVisible) {
+      htmlElement.style.display = "block";
+    } else {
+      htmlElement.style.display = "none";
+    }
+  }
+
+  updateAddElement(formElement: HTMLElement, index: number): void {
+    var addWrapper = formElement.querySelector(
+      this.addElementWrapperSelector
+    ) as HTMLElement;
+    var isVisible = index == this.getNumberOfTotalForms();
+    this.setVisibility(addWrapper, isVisible);
+  }
+
+  updateDeleteElement(formElement: HTMLElement, index: number): void {
+    var deleteWrapper = formElement.querySelector(
+      this.deleteElementWrapperSelector
+    ) as HTMLElement;
+
+    var numberOfMinForms = this.getNumberOfMinForms();
+    var numberOfTotalForms = this.getNumberOfTotalForms();
+
+    var isVisible =
+      index <= numberOfMinForms && numberOfTotalForms <= numberOfMinForms;
+    this.setVisibility(deleteWrapper, isVisible);
+  }
+
+  updateOrderElement(formElement: HTMLElement, index: number): void {
+    var orderWrapper = formElement.querySelector(
+      this.orderElementWrapperSelector
+    ) as HTMLElement;
+
+    var beforeOrderElement = orderWrapper.querySelector(
+      this.orderElementBeforeSelector
+    ) as HTMLElement;
+    var isBeforeVisible = index != 1;
+    this.setVisibility(beforeOrderElement, isBeforeVisible);
+
+    var numberOfTotalForms = this.getNumberOfTotalForms();
+
+    var afterOrderElement = orderWrapper.querySelector(
+      this.orderElementAfterSelector
+    ) as HTMLElement;
+    var isAfterVisible = index == numberOfTotalForms;
+    this.setVisibility(afterOrderElement, isAfterVisible);
+  }
 }
